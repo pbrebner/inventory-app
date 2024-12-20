0076544186fae5bd9a7ec681dd91695a1e338bcf
Patrick Brebner <patrick.brebner@gmail.com>
2024-12-19 17:41:03 -0800

    Update DB queries

--- db/queries.js
+++ db/queries.js
@@ -3,76 +3,93 @@ const pool = require("./pool");
 // Movie Queries
 
 async function selectMovies() {
-    await pool.query("SELECT * FROM movies");
+    // Deconstruction to get the rows value from the query object (returns list of objects)
+    const { rows } = await pool.query("SELECT * FROM movies");
+    return rows;
 }
 
-async function insertMovie(movie) {
-    await pool.query("INSERT INTO movies (title) VALUES ($1)", [movie]);
+async function insertMovie(title, yr_released) {
+    await pool.query(
+        "INSERT INTO movies (title, yr_released) VALUES ($1, $2)",
+        [title, yr_released]
+    );
 }
 
-async function selectMovie(movie) {
-    await pool.query("SELECT * FROM movies WHERE title = ($1)", [movie]);
-}
-
-async function updateMovie(newMovie, oldMovie) {
-    await pool.query("UPDATE movies SET title = ($1) WHERE title = ($2)", [
-        newMovie,
-        oldMovie,
+async function selectMovie(movieId) {
+    const { rows } = await pool.query("SELECT * FROM movies WHERE id = ($1)", [
+        movieId,
     ]);
+    return rows;
 }
 
-async function deleteMovie(movie) {
-    await pool.query("DELETE FROM movies WHERE title = ($1)", [movie]);
+async function updateMovie(movieId, title, yr_released) {
+    await pool.query(
+        "UPDATE movies SET title = ($2), yr_released = ($3) WHERE id = ($1)",
+        [movieId, title, yr_released]
+    );
+}
+
+async function deleteMovie(movieId) {
+    await pool.query("DELETE FROM movies WHERE id = ($1)", [movieId]);
 }
 
 // Genre Queries
 
 async function selectGenres() {
-    await pool.query("SELECT * FROM genres");
+    const { rows } = await pool.query("SELECT * FROM genres");
+    return rows;
 }
 
 async function insertGenre(genre) {
     await pool.query("INSERT INTO genres (genre) VALUES ($1)", [genre]);
 }
 
-async function selectGenre(genre) {
-    await pool.query("SELECT * FROM genres WHERE genre = ($1)", [genre]);
+async function selectGenre(genreId) {
+    const { rows } = await pool.query("SELECT * FROM genres WHERE id = ($1)", [
+        genreId,
+    ]);
+    return rows;
 }
 
-async function updateGenre(newGenre, oldGenre) {
-    await pool.query("UPDATE genres SET genre = ($1) WHERE genre = ($2)", [
-        newGenre,
-        oldGenre,
+async function updateGenre(genreId, genre) {
+    await pool.query("UPDATE genres SET genre = ($2) WHERE id = ($1)", [
+        genreId,
+        genre,
     ]);
 }
 
-async function deleteGenre(genre) {
-    await pool.query("DELETE FROM genres WHERE genre = ($1)", [genre]);
+async function deleteGenre(genreId) {
+    await pool.query("DELETE FROM genres WHERE id = ($1)", [genreId]);
 }
 
 // Director Queries
 
 async function selectDirectors() {
-    await pool.query("SELECT * FROM directors");
+    const { rows } = await pool.query("SELECT * FROM directors");
+    return rows;
 }
 
-async function insertDirector(director) {
-    await pool.query("INSERT INTO directors (name) VALUES ($1)", [director]);
+async function insertDirector(name) {
+    await pool.query("INSERT INTO directors (name) VALUES ($1)", [name]);
 }
 
-async function selectDirector(director) {
-    await pool.query("SELECT * FROM directors WHERE name = ($1)", [director]);
+async function selectDirector(directorId) {
+    const { rows } = await pool.query(
+        "SELECT * FROM directors WHERE id = ($1)",
+        [directorId]
+    );
+    return rows;
 }
 
-async function updateDirector(newDirector, oldDirector) {
-    await pool.query("UPDATE directors SET name = ($1) WHERE name = ($2)", [
-        newDirector,
-        oldDirector,
+async function updateDirector(directorId, name) {
+    await pool.query("UPDATE directors SET name = ($2) WHERE id = ($1)", [
+        directorId,
+        name,
     ]);
 }
 
-async function deleteDirector(director) {
-    await pool.query("DELETE FROM directors WHERE name = ($1)", [director]);
+async function deleteDirector(directorId) {
+    await pool.query("DELETE FROM directors WHERE id = ($1)", [directorId]);
 }
 
 module.exports = {
