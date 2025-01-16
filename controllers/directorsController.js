cd155c68e419bb59ede09711c0e40446223cb065
Patrick Brebner <patrick.brebner@gmail.com>
2025-01-16 14:57:05 -0800

    Update to show movies

--- controllers/directorsController.js
+++ controllers/directorsController.js
@@ -27,11 +27,11 @@ exports.createDirector = [
     validateDirector,
     asyncHandler(async (req, res, next) => {
         const errors = validationResult(req);
+        // Don't like having to query again
+        let directors = await db.selectDirectors();
 
         if (!errors.isEmpty()) {
             // Error Occured
-            // Don't like having to query again
-            let directors = await db.selectDirectors();
             res.render("directors", {
                 title: "All Directors",
                 directors: directors,
@@ -41,8 +41,21 @@ exports.createDirector = [
                 errors: errors.array(),
             });
         } else {
-            await db.insertDirector(req.body.name);
-            res.redirect("/directors");
+            let director = await db.selectDirectorByName(req.body.name);
+
+            if (director.length != 0) {
+                res.render("directors", {
+                    title: "All Directors",
+                    directors: directors,
+                    directorEntry: {
+                        name: req.body.name,
+                    },
+                    errors: [{ msg: "Genre already exists" }],
+                });
+            } else {
+                await db.insertDirector(req.body.name);
+                res.redirect("/directors");
+            }
         }
     }),
 ];
@@ -50,9 +63,11 @@ exports.createDirector = [
 // Get Director
 exports.getDirector = asyncHandler(async (req, res, next) => {
     let director = await db.selectDirector(req.params.directorId);
+    let directorMovies = await db.selectDirectorMovies(req.params.directorId);
     res.render("director", {
         title: `${director[0].name}`,
         director: director[0],
+        movies: directorMovies,
     });
 });
 
@@ -61,12 +76,13 @@ exports.editDirector = asyncHandler(async (req, res, next) => {
     let director = await db.selectDirector(req.params.directorId);
     res.render("editDirector", {
         title: "Edit Director",
-        director: director[0],
+        director: director[0].name,
         errors: [],
     });
 });
 
 // Update Director
+// TODO: Figure out if movies should keep the updated director
 exports.updateDirector = [
     validateDirector,
     asyncHandler(async (req, res, next) => {
@@ -86,6 +102,7 @@ exports.updateDirector = [
 ];
 
 // Delete Director
+// TODO: Set director to NULL on delete?
 exports.deleteDirector = asyncHandler(async (req, res, next) => {
     await db.deleteDirector(req.params.directorId);
     res.redirect("/directors");
