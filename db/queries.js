f191f32902d3c6f4e3f9bf903982cea6fb672eaf
Patrick Brebner <patrick.brebner@gmail.com>
2025-01-15 17:33:45 -0800

    Update queries with new inputs and return ids

--- db/queries.js
+++ db/queries.js
@@ -8,40 +8,37 @@ async function selectMovies() {
     return rows;
 }
 
-async function insertMovie(title, yr_released, rating) {
-    await pool.query(
-        "INSERT INTO movies (title, yr_released, rating) VALUES ($1, $2, $3)",
-        [title, yr_released, rating]
+async function insertMovie(title, yr_released, rating, director_id) {
+    const { rows } = await pool.query(
+        "INSERT INTO movies (title, yr_released, rating, director_id) VALUES ($1, $2, $3, $4) RETURNING id",
+        [title, yr_released, rating, director_id]
     );
+    return rows;
 }
 
 async function selectMovie(movieId) {
     // Gets movie, including director name and director_id
     const { rows } = await pool.query(
-        "SELECT movies.id, title, yr_released, rating, director_id, name FROM movies JOIN directors ON director_id = director.id WHERE movies.id = ($1)",
+        "SELECT movies.id, title, yr_released, rating, director_id, name AS director FROM movies JOIN directors ON director_id = directors.id WHERE movies.id = ($1)",
         [movieId]
     );
     return rows;
 }
 
-async function selectMovieGenre(movieId) {
-    // Gets genre of specific movie
+async function updateMovie(movieId, title, yr_released, rating, directorId) {
     const { rows } = await pool.query(
-        "SELECT * FROM genres WHERE id IN (SELECT genre_id FROM movieGenres WHERE movie_id = ($1))",
-        [movieId]
+        "UPDATE movies SET title = ($2), yr_released = ($3), rating = ($4), director_id = ($5) WHERE id = ($1) RETURNING id",
+        [movieId, title, yr_released, rating, directorId]
     );
     return rows;
-}
-
-async function updateMovie(movieId, title, yr_released, rating) {
-    await pool.query(
-        "UPDATE movies SET title = ($2), yr_released = ($3), rating = ($4) WHERE id = ($1)",
-        [movieId, title, yr_released, rating]
-    );
 }
 
 async function deleteMovie(movieId) {
-    await pool.query("DELETE FROM movies WHERE id = ($1)", [movieId]);
+    const { rows } = await pool.query(
+        "DELETE FROM movies WHERE id = ($1) RETURNING id",
+        [movieId]
+    );
+    return rows;
 }
 
 // GENRE QUERIES
@@ -52,13 +49,98 @@ async function selectGenres() {
 }
 
 async function insertGenre(genre) {
-    await pool.query("INSERT INTO genres (genre) VALUES ($1)", [genre]);
+    const { rows } = await pool.query(
+        "INSERT INTO genres (genre) VALUES ($1) RETURNING id",
+        [genre]
+    );
+    return rows;
 }
 
 async function selectGenre(genreId) {
     const { rows } = await pool.query("SELECT * FROM genres WHERE id = ($1)", [
         genreId,
     ]);
+    return rows;
+}
+
+async function selectGenreByGenre(genre) {
+    const { rows } = await pool.query(
+        "SELECT * FROM genres WHERE genre = ($1)",
+        [genre]
+    );
+    return rows;
+}
+
+async function updateGenre(genreId, genre) {
+    const { rows } = await pool.query(
+        "UPDATE genres SET genre = ($2) WHERE id = ($1) RETURNING id",
+        [genreId, genre]
+    );
+    return rows;
+}
+
+async function deleteGenre(genreId) {
+    const { rows } = await pool.query(
+        "DELETE FROM genres WHERE id = ($1) RETURNING id",
+        [genreId]
+    );
+    return rows;
+}
+
+// DIRECTOR QUERIES
+
+async function selectDirectors() {
+    const { rows } = await pool.query("SELECT * FROM directors");
+    return rows;
+}
+
+async function insertDirector(name) {
+    const { rows } = await pool.query(
+        "INSERT INTO directors (name) VALUES ($1) RETURNING id",
+        [name]
+    );
+    return rows;
+}
+
+async function selectDirector(directorId) {
+    const { rows } = await pool.query(
+        "SELECT movies.id, title, yr_released, rating, director_id, name AS director FROM movies JOIN directors ON director_id = directors.id WHERE directors.id = ($1)",
+        [directorId]
+    );
+    return rows;
+}
+async function selectDirectorByName(name) {
+    const { rows } = await pool.query(
+        "SELECT * FROM directors WHERE name = ($1)",
+        [name]
+    );
+    return rows;
+}
+
+async function updateDirector(directorId, name) {
+    const { rows } = await pool.query(
+        "UPDATE directors SET name = ($2) WHERE id = ($1) RETURNING id",
+        [directorId, name]
+    );
+    return rows;
+}
+
+async function deleteDirector(directorId) {
+    const { rows } = await pool.query(
+        "DELETE FROM directors WHERE id = ($1) RETURNING id",
+        [directorId]
+    );
+    return rows;
+}
+
+// MOVIEGENRE QUERIES
+
+async function selectMovieGenre(movieId) {
+    // Gets genre of specific movie
+    const { rows } = await pool.query(
+        "SELECT * FROM genres WHERE id IN (SELECT genre_id FROM movieGenres WHERE movie_id = ($1))",
+        [movieId]
+    );
     return rows;
 }
 
@@ -71,72 +153,43 @@ async function selectGenreMovie(genreId) {
     return rows;
 }
 
-async function updateGenre(genreId, genre) {
-    await pool.query("UPDATE genres SET genre = ($2) WHERE id = ($1)", [
-        genreId,
-        genre,
-    ]);
-}
-
-async function deleteGenre(genreId) {
-    await pool.query("DELETE FROM genres WHERE id = ($1)", [genreId]);
-}
-
-// DIRECTOR QUERIES
-
-async function selectDirectors() {
-    const { rows } = await pool.query("SELECT * FROM directors");
-    return rows;
-}
-
-async function insertDirector(name) {
-    await pool.query("INSERT INTO directors (name) VALUES ($1)", [name]);
-}
-
-async function selectDirector(directorId) {
+async function insertMovieGenre(movieId, genreId) {
     const { rows } = await pool.query(
-        "SELECT * FROM directors WHERE id = ($1)",
-        [directorId]
+        "INSERT INTO moviegenres (movie_id, genre_id) VALUES ($1, $2) RETURNING id",
+        [movieId, genreId]
     );
     return rows;
 }
 
-async function selectDirectorMovie(directorId) {
+// Only works with movies having one genre
+async function updateMovieGenre(movieId, genreId) {
     const { rows } = await pool.query(
-        "SELECT movies.id, title, yr_released, rating, director_id, name FROM movies JOIN directors ON director_id = director.id WHERE diectors.id = ($1)",
-        [directorId]
+        "UPDATE moviegenres SET genre_id = ($2) WHERE movie_id = ($1) RETURNING id",
+        [movieId, genreId]
     );
     return rows;
-}
-
-async function updateDirector(directorId, name) {
-    await pool.query("UPDATE directors SET name = ($2) WHERE id = ($1)", [
-        directorId,
-        name,
-    ]);
-}
-
-async function deleteDirector(directorId) {
-    await pool.query("DELETE FROM directors WHERE id = ($1)", [directorId]);
 }
 
 module.exports = {
     selectMovies,
     insertMovie,
     selectMovie,
-    selectMovieGenre,
     updateMovie,
     deleteMovie,
     selectGenres,
     insertGenre,
     selectGenre,
-    selectGenreMovie,
+    selectGenreByGenre,
     updateGenre,
     deleteGenre,
     selectDirectors,
     insertDirector,
     selectDirector,
-    selectDirectorMovie,
+    selectDirectorByName,
     updateDirector,
     deleteDirector,
+    selectMovieGenre,
+    selectGenreMovie,
+    insertMovieGenre,
+    updateMovieGenre,
 };
